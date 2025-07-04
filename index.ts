2c03953f7e684f0492f7b7a28cc2ed02b0460d52
martin visuara <martinvisuara10@gmail.com>
2025-07-04 02:35:30 -0300

    otro cambio

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
- class Product {
+class Product {
   constructor(name: string, price: number) {
     this.name = name;
     this.price = price;
@@ -10,26 +10,23 @@
   }
 }
 
-
 class ProductoAlimenticio extends Product {
   fechaCaducidad: Date;
   constructor(name, price, fechaCaducidad) {
     super(name, price);
     // guarda la fecha de caducidad
-    this.fechaCaducidad = new Date(fechaCaducidad)
+    this.fechaCaducidad = new Date(fechaCaducidad);
   }
 
-  checkCaducidad():boolean {
+  checkCaducidad(): boolean {
     const hoy = new Date();
     // [Completa la lógica para obtener si el producto está vencido]
     return this.fechaCaducidad < hoy;
   }
 }
 
-
-
 // Creación de la instancia
-const pan = new ProductoAlimenticio("Pan Integral", 3.50, "2024-05-30");
+const pan = new ProductoAlimenticio("Pan Integral", 3.5, "2024-05-30");
 
 const estaVencido = pan.checkCaducidad();
 console.log("¿El producto está vencido?:", estaVencido ? "Sí" : "No");
@@ -38,3 +35,4 @@ console.log("¿El producto está vencido?:", estaVencido ? "Sí" : "No");
 console.log("Nombre del Producto:", pan.name);
 console.log("Precio:", pan.getPrice());
 console.log("Fecha de Caducidad:", pan.fechaCaducidad);
+console.log("otro commit");
